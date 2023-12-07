9aeeea78b7ea9954ca9a787a62a163724f17a811
Liseon617 <64420980+Liseon617@users.noreply.github.com>
2023-12-08 02:17:51 +0800

    delete and save function for created graph

--- js/math/graph.js
+++ js/math/graph.js
@@ -4,6 +4,18 @@ class Graph {
         this.segments = segments;
     }
 
+    static load(info){
+        const points = info.points.map((i) => new Point(i.x, i.y));
+        // have the segments be connected to their points
+        const segments = info.segments.map((i) => new Segment(
+            points.find((p) => p.equals(i.p1)),
+            points.find((p) => p.equals(i.p2))
+            )
+        );
+
+        return new Graph(points, segments)
+    }
+
     addPoint(point) {
         this.points.push(point);
     }
--- js/viewport.js
+++ js/viewport.js
@@ -4,7 +4,9 @@ class Viewport {
         this.ctx = canvas.getContext("2d");
 
         this.zoom = 1;
-        this.offset = new Point(0, 0);
+        this.center = new Point(canvas.width / 2, canvas.height / 2);
+        //scale offset: -canvas height / 2 and width / 2
+        this.offset = scale(this.center, -1);
 
         this.drag = {
             start: new Point(0, 0),
@@ -16,11 +18,27 @@ class Viewport {
         this.#addEventListeners();
     }
 
-    getMouse(evt){
-        return new Point(
-            evt.offsetX * this.zoom,
-            evt.offsetY * this.zoom
+    reset(){
+        this.ctx.restore();
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.ctx.save();
+        this.ctx.translate(this.center.x, this.center.y);
+        this.ctx.scale(1 / this.zoom, 1 / this.zoom);
+        const offset = this.getOffset();
+        this.ctx.translate(offset.x, offset.y);
+    }
+
+
+    getMouse(evt, substractDragOffset = false){
+        const p = new Point(
+            (evt.offsetX - this.center.x) * this.zoom - this.offset.x,
+            (evt.offsetY - this.center.y) * this.zoom - this.offset.y
         );
+        return substractDragOffset ? substract(p, this.drag.offset) : p;
+    }
+
+    getOffset(){
+        return add(this.offset, this.drag.offset);
     }
 
     #addEventListeners() {
@@ -39,26 +57,31 @@ class Viewport {
     }
 
     #handleMouseDown(evt){
-        const dir = Math.sign(evt.deltaY);
-        const step = 0.1;
-        this.zoom += dir * step;
-        //keep zoom range between 1 - 5
-        this.zoom = Math.max(1, Math.min(5, this.zoom)); 
+        if(evt.button == 1){//middle button clicked
+            this.drag.start = this.getMouse(evt);
+            this.drag.active = true;
+        }
     }
 
     #handleMouseMove(evt){
-        const dir = Math.sign(evt.deltaY);
-        const step = 0.1;
-        this.zoom += dir * step;
-        //keep zoom range between 1 - 5
-        this.zoom = Math.max(1, Math.min(5, this.zoom)); 
+        if(this.drag.active){
+            //end location of current action
+            //end - start = offset of current drag
+            this.drag.end = this.getMouse(evt);
+            this.drag.offset = substract(this.drag.end, this.drag.start);
+        }
     }
 
     #handleMouseUp(evt){
-        const dir = Math.sign(evt.deltaY);
-        const step = 0.1;
-        this.zoom += dir * step;
-        //keep zoom range between 1 - 5
-        this.zoom = Math.max(1, Math.min(5, this.zoom)); 
+        if(this.drag.active){
+            this.offset = add(this.offset, this.drag.offset);
+            //reset drag information
+            this.drag = {
+                start: new Point(0, 0),
+                end: new Point(0, 0),
+                offset: new Point(0, 0),
+                active: false
+            };
+        }
     }
 }
