88fadcbe9518803998880d0e687cfffc043c24ba
Liseon617 <64420980+Liseon617@users.noreply.github.com>
2023-12-10 05:04:54 +0800

    building generation

--- js/primitives/segment.js
+++ js/primitives/segment.js
@@ -3,6 +3,14 @@ class Segment {
         this.p1 = p1;
         this.p2 = p2;
     }
+    
+    length(){
+        return distance(this.p1, this.p2);
+    }
+
+    directionVector(){
+        return normalize(substract(this.p2, this.p1));
+    }
 
     equals(seg){
         return this.includes(seg.p1) && this.includes(seg.p2)
