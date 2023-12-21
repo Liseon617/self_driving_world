5c43319bca1071aeeb82e161f1eab4a836de5bed
Liseon617 <64420980+Liseon617@users.noreply.github.com>
2023-12-22 01:27:15 +0800

    minimap and osm integration

--- world/js/math/utils.js
+++ world/js/math/utils.js
@@ -1,28 +1,28 @@
 function getNearestPoint(loc, points, threshold = Infinity) {
-    let minDist = Infinity;
-    let nearest = null;
-    for (const point of points) {
-       const dist = distance(point, loc);
-       if (dist < minDist && dist < threshold) {
-          minDist = dist;
-          nearest = point;
-       }
+  let minDist = Infinity;
+  let nearest = null;
+  for (const point of points) {
+    const dist = distance(point, loc);
+    if (dist < minDist && dist < threshold) {
+      minDist = dist;
+      nearest = point;
     }
-    return nearest;
- }
+  }
+  return nearest;
+}
 
 function getNearestSegment(loc, segments, threshold = Number.MAX_SAFE_INTEGER) {
-    let minDist = Number.MAX_SAFE_INTEGER;
-    let nearest = null;
-    for (const seg of segments) {
-       const dist = seg.distanceToPoint(loc);
-       if (dist < minDist && dist < threshold) {
-          minDist = dist;
-          nearest = seg;
-       }
+  let minDist = Number.MAX_SAFE_INTEGER;
+  let nearest = null;
+  for (const seg of segments) {
+    const dist = seg.distanceToPoint(loc);
+    if (dist < minDist && dist < threshold) {
+      minDist = dist;
+      nearest = seg;
     }
-    return nearest;
- }
+  }
+  return nearest;
+}
 
 function distance(p1, p2) {
   return Math.hypot(p1.x - p2.x, p1.y - p2.y);
@@ -57,8 +57,8 @@ function magnitude(p) {
 }
 
 function perpendicular(p) {
-    return new Point(-p.y, p.x);
- }
+  return new Point(-p.y, p.x);
+}
 
 function translate(loc, angle, offset) {
   return new Point(
@@ -98,6 +98,14 @@ function lerp(a, b, t) {
 
 function lerp2D(A, B, t) {
   return new Point(lerp(A.x, B.x, t), lerp(A.y, B.y, t));
+}
+
+function invLerp(a, b, v) {
+  return (v - a) / (b - a);
+}
+
+function degToRad(degree) {
+  return (degree * Math.PI) / 180;
 }
 
 function getRandomColor() {
